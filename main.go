3ead3286e3d85d6ced21a6fc0d42f72fc01821f4
John Shenk <john_shenk@hotmail.com>
2017-01-14 16:41:05 -0600

    copies working proj

--- main.go
+++ main.go
@@ -1,28 +1,61 @@
+// package main
+
+// import (
+// 	"log"
+// 	"net/http"
+// 	"os"
+
+// 	"github.com/stinkyfingers/catchphraseAPI/handlers"
+// )
+
+// func main() {
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		log.Fatal("$PORT must be set")
+// 	}
+
+// 	s, err := handlers.NewServer()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/", s.HandleStatus)
+// 	mux.HandleFunc("/all", s.Cors(s.HandleAll))
+// 	mux.HandleFunc("/upload", s.Cors(s.HandleUpload))
+// 	log.Print("Running on port ", port)
+// 	log.Fatal(http.ListenAndServe(":"+port, mux))
+// }
+
 package main
 
 import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/stinkyfingers/catchphraseAPI/handlers"
+	// "github.com/gin-gonic/gin"
 )
 
 func main() {
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	s, err := handlers.NewServer()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
 
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
+
+	// router.Run(":" + port)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", s.HandleStatus)
-	mux.HandleFunc("/all", s.Cors(s.HandleAll))
-	mux.HandleFunc("/upload", s.Cors(s.HandleUpload))
-	log.Print("Running on port ", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hey"))
+	})
+	http.ListenAndServe(":"+port, mux)
 }
